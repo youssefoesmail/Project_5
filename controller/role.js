1dc747c5d1e21d60c8648f58fe47263b04f7b154
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-02-12 19:50:42 +0300

    role Router slove

--- controller/role.js
+++ controller/role.js
@@ -23,14 +23,6 @@ const createNewPermission = (req, res) => {
         });
       });
   };
-
-  module.exports = {
-    createNewPermission,
-  };
-  
-
-const pool = require("../models/db");
-
 const createNewRole = (req, res) => {
   const { role } = req.body;
 
@@ -42,29 +34,6 @@ const createNewRole = (req, res) => {
       res.status(201).json({
         success: true,
         message: "Role created successfully",
-        result: result.rows,
-      });
-    })
-    .catch((err) => {
-      res.status(500).json({
-        success: false,
-        message: `Server error`,
-        err: err,
-      });
-    });
-};
-
-const createNewPermission = (req, res) => {
-  const { permission } = req.body;
-  const query = `INSERT INTO permissions (permission) VALUES ($1) RETURNING *;`;
-  const data = [permission];
-
-  pool
-    .query(query, data)
-    .then((result) => {
-      res.status(201).json({
-        success: true,
-        message: `Permission created successfully`,
         result: result.rows,
       });
     })
