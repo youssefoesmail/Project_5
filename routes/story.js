f3167e903b1c7871ff891b38d9b40a9b599f940c
youssefoesmail <youssefo.esmail@gmail.com>
2024-02-12 21:01:43 +0300

    deleteStory

--- routes/story.js
+++ routes/story.js
@@ -1,9 +1,13 @@
-
-const express = require('express');
-const { getAllStories,createNewStory } = require('../controller/story');
+const express = require("express");
+const {
+  getAllStories,
+  createNewStory,
+  deleteStoryById,
+} = require("../controller/story");
 const storyRouter = express.Router();
 storyRouter.post("/", createNewStory);
-storyRouter.get('/',getAllStories);
+storyRouter.get("/", getAllStories);
+storyRouter.delete("/:id", deleteStoryById);
 storyRouter.use("*", (req, res) => {
   res.json("storyRouter is working");
 });
