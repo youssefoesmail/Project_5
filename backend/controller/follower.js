cd7f0ec01a56a63e6cf5ddd40e6f8ce1479ae1e6
Malekqandeel <malikqan@gmail.com>
2024-02-14 23:10:23 +0300

    Function was created and checked on followers

--- backend/controller/follower.js
+++ backend/controller/follower.js
@@ -44,8 +44,30 @@ const deleteFollower = (req,res)=>{
       });
 };
 
+const getFollowersUser = (req,res)=>{
+    const followed_user_id = req.params.id;
+    const values = [followed_user_id];
+    const query = `select * from follows WHERE followed_user_id=$1`
+
+    pool.query(query,values).then((result) => {
+        res.status(200).json({
+          success: true,
+          message: `followers for user ${followed_user_id}`,
+          result: result.rows[0]
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          success: false,
+          message: "Server error",
+          err: err
+        });
+      });
+}
+
 
 module.exports = {
     createFollower,
     deleteFollower,
+    getFollowersUser,
 }
--- backend/routes/follower.js
+++ backend/routes/follower.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { createFollower, deleteFollower } = require('../controller/follower');
+const { createFollower, deleteFollower, getFollowersUser } = require('../controller/follower');
 const authentication = require('../middleware/Authentication');
 
 const followerRouter = express.Router();
 
 followerRouter.post('/:id',authentication,createFollower);
 followerRouter.delete('/:id',authentication,deleteFollower);
+followerRouter.get('/userFollowers/:id',authentication,getFollowersUser);
 
 
 module.exports= followerRouter;
