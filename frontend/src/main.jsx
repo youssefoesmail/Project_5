16bd79548c5a158dc5c634bf85e4e212a794fdbc
youssefoesmail <youssefo.esmail@gmail.com>
2024-02-14 22:41:14 +0300

    login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,6 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
-      <App />
     </Provider>
   </React.StrictMode>
 );
