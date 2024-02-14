963f5dba0c98cad455399719f05934cad06bc888
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-02-14 12:09:12 +0300

    getCommentsByPostId

--- controller/comments.js
+++ controller/comments.js
@@ -26,6 +26,31 @@ const createNewCommentPost = (req, res) => {
       });
     });
 };
+const getCommentByPostId = (req, res) => {
+  const { id } = req.params;
+  const query = `SELECT comment_posts.comment, comment_posts.post_id, users.firstName, comment_posts.commenter
+  FROM comment_posts
+  JOIN users ON users.id = comment_posts.commenter
+  WHERE comment_posts.post_id =$1 AND comment_posts.is_deleted = 0;`;
+  const value = [id];
+  pool
+    .query(query, value)
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        message: `All comments for posts: ${id}`,
+        result: result.rows
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: "Server error",
+        err: err
+      });
+    });
+};
+
 const createNewCommentStory = (req, res) => {
   const story_id = req.params.id;
   const commenter = req.token.userId;
