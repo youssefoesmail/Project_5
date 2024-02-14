dc28f36686273c9ed0793bd9aadf1ccf385ab28b
Rashedalfoqha <rashedmohammadalfoqha@gmail.com>
2024-02-14 23:45:25 +0300

    updatePost

--- frontend/src/components/redux/post/postSlice.jsx
+++ frontend/src/components/redux/post/postSlice.jsx
@@ -7,8 +7,19 @@ const postSlice = createSlice({
   reducers: {
     setPosts: (state, action) => {
       state.posts = action.payload;
+    },
+    createNewPost: (state, action) => {
+      state.posts.unshift(action.payload);
+    },
+    updatePostById: (state, action) => {
+      state.posts = state.posts.map((elem, ind) => {
+        if (elem.id === action.payload.id) {
+          return { ...elem, ...action.payload };
+        }
+        return elem;
+      });
     }
   }
 });
-export const { setPosts } = postSlice.actions;
+export const { setPosts, createNewPost, updatePostById } = postSlice.actions;
 export default postSlice.reducer;
