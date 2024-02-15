df31fac32ec25324a63f334adb2b778fd791d17b
youssefoesmail <youssefo.esmail@gmail.com>
2024-02-15 22:33:00 +0300

    k

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
 
     const history = useNavigate();
 
-    //================= useState ========================
+    //================= useState =========================
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
