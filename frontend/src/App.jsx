20c003259b192a4f08cd07e2086acf4a31e5e7e3
youssefoesmail <youssefo.esmail@gmail.com>
2024-02-16 17:38:14 +0300

    comment component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import Posts from "./components/posts/Posts";
 import { Routes, Route } from "react-router-dom";
 
 import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Comments from "./components/Comments/Comments";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/post" element={<Posts />} />
           <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/comments" element={<Comments />} />
         </Routes>
       </div>
     </>
