68d913ee9d2e339a161b6a2a36276d4899df365d
Malekqandeel <malikqan@gmail.com>
2024-02-19 22:17:47 +0300

    Followers Page is under cooking

--- frontend/src/components/FollowPost/FollowPost.jsx
+++ frontend/src/components/FollowPost/FollowPost.jsx
@@ -2,30 +2,52 @@ import React from 'react'
 import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { setFollowers } from '../redux/personalPage/personal';
+import { setFollowers, setPosts } from '../redux/personalPage/personal';
 
 const FollowPost = () => {
     const dispatch = useDispatch();
-    const { auth, personal, post } = useSelector((state) => {
+    const { auth, personal, post,followers } = useSelector((state) => {
         return {
           auth: state.auth,
           post: state.post,
-          personal: state.personal.personal
+          personal: state.personal.personal,
+          followers: state.personal.followers
         };
       });
-    console.log(auth.userId);
+    console.log(auth.userId,auth.token);
     useEffect(()=>{
-        axios.get(`http://localhost:5000/followers/userFollowers/1`,{headers: {
-            authorization: `Bearer ${auth.token}`
-          }})
+        axios.get(`http://localhost:5000/followers/userFollowers/${auth.userId}`, {
+            headers: {
+              authorization: `Bearer ${auth.token}`
+            }
+          })
         .then((result) => {
-            console.log(result);
+            console.log(result.data.result);
             dispatch(setFollowers(result.data))
           })
           .catch((err) => {
             console.log(err);
           });
-    },[])
+
+        console.log(followers);
+    axios.get(`http://localhost:5000/posts/search_1/${followers.result.following_user_id}`,
+            {
+            headers: {
+              authorization: `Bearer ${auth.token}`
+            }
+          })
+        .then((result) => {
+            console.log(result.data.result);
+            //dispatch(setPosts(result))
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+    }
+    
+    ,[]);
+    
+    
   return (
     <div>FollowPost</div>
   )
