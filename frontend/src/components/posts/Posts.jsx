924713ea4a969b9f9fb2855afd02ca590bbfd271
youssefoesmail <youssefo.esmail@gmail.com>
2024-02-28 15:10:06 +0300

    commentShow

--- frontend/src/components/posts/Posts.jsx
+++ frontend/src/components/posts/Posts.jsx
@@ -435,6 +435,8 @@ const Posts = () => {
                             </svg>
                             <span>12</span>
                           </div>
+
+
                           <div class="flex mr-2 text-gray-700 text-sm mr-6 hover:bg-blue-300">
                             <svg
                               fill="none"
@@ -442,8 +444,12 @@ const Posts = () => {
                               class="w-10 h-10 mr-1"
                               stroke="currentColor"
                               onClick={() => {
-                                setOpenModal(true)
+                                setOpenModal(elem.id);
                                 getPostComment(elem.id);
+                                console.log(elem.user_id, show);
+                                setShow(elem.user_id);
+                                { elem.id != postId ? setPostId(elem.id) : setPostId("") }
+                                setInfo(elem.comment)
                               }}
                             // onClick={() => {
                             //   getPostComment(elem.id);
@@ -461,6 +467,8 @@ const Posts = () => {
                             </svg>
                             <span>{elem.comment?.length}</span>
                           </div>
+
+
                           <div class="flex mr-2 text-gray-700 text-sm mr-6 hover:bg-blue-300">
                             <svg
                               fill="none"
@@ -514,36 +522,7 @@ const Posts = () => {
 
                           <br />
                           <>
-                            <div class="flex mr-2 text-gray-700 text-sm mr-6 hover:bg-blue-300">
-                              <svg
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                class="w-10 h-10 mr-1"
-                                stroke="currentColor"
-                                onClick={() => {
-                                  setOpenModal(elem.id);
-                                  getPostComment(elem.id);
-                                  setShow(elem.user_id);
-                                  setPostId(elem.id);
-                                  setInfo(elem.comment)
 
-                                }}
-                              // onClick={() => {
-                              //   getPostComment(elem.id);
-
-                              //   setShow(elem.user_id);
-                              //   setPostId(elem.id);
-                              // }}
-                              >
-                                <path
-                                  stroke-linecap="round"
-                                  stroke-linejoin="round"
-                                  stroke-width="2"
-                                  d="M17 8h2a2 2 0 012 2v6a2 2 0 01-2 2h-2v4l-4-4H9a1.994 1.994 0 01-1.414-.586m0 0L11 14h4a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2v4l.586-.586z"
-                                />
-                              </svg>
-                              <span>{elem.comment?.length}</span>
-                            </div>
 
                           </>
                           {/* <Modal show={openModal}  onClose={() => { setOpenModal("") }}>
