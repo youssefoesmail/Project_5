1774a7640f7a100f920df861b0eb06d194d0ba72
youssefoesmail <youssefo.esmail@gmail.com>
2024-02-29 20:07:54 +0300

    adding sideBare

--- frontend/src/components/posts/Posts.jsx
+++ frontend/src/components/posts/Posts.jsx
@@ -427,8 +427,65 @@ const Posts = () => {
           </div>
           {posts?.map((elem) => {
             return (
-              <>
-                <div class="flex bg-white drop-shadow-2xl rounded-lg my-6">
+              <div class="flex ">
+                <div class="container px-6 py-10 mx-auto w-1/4">
+                  <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+                    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+                      <ul class="space-y-2 font-medium">
+                        <li>
+                          <Link
+                            to={`/users/${elem.user_id}`}
+                            onClick={() => {
+                              dispatch(setUserId(elem.user_id));
+                            }}
+                          >
+                            <a href="/users" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                              {elem.photo ? <img
+                                class="object-cover w-16 h-16 rounded-full"
+                                src={elem.photo || photo}
+                                alt=""
+                              /> : <img
+                                class="object-cover w-16 h-16 rounded-full"
+                                src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o="
+                                alt=""
+                              />}
+                              <span class="ms-3">{elem.firstname} {elem.lastname}</span>
+                            </a>
+                          </Link>
+                        </li>
+                        <li>
+                          <a href="/" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                              <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                            </svg>
+                            <span class="flex-1 ms-3 whitespace-nowrap">friends</span>
+
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 9h0M9 9h0m12 3a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM7 13c0 1 .5 2.4 1.5 3.2a5.5 5.5 0 0 0 7 0c1-.8 1.5-2.2 1.5-3.2 0 0-2 1-5 1s-5-1-5-1Z" />
+                            </svg>
+                            <span class="flex-1 ms-3 whitespace-nowrap">Games</span>
+                            <span class="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium text-gray-800 bg-gray-100 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                            <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                              <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z" />
+                              <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z" />
+                              <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z" />
+                            </svg>
+                            <span class="flex-1 ms-3 whitespace-nowrap">Logout</span>
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </aside>
+                </div>
+                <div class="flex bg-white drop-shadow-2xl rounded-lg my-6 w-4/4">
                   <div class="flex flex-col px-4 py-6 my-6 mx-6">
                     <div className="" key={elem.id}>
                       <>
@@ -502,7 +559,7 @@ const Posts = () => {
                           </video>
                         )}
                         <div class="mt-4 flex items-center px-4 py-6 my-6 mx-6">
-                          <div class="flex mr-12 text-gray-700 text-sm mr-6 hover:bg-red-300" onClick={()=>{createLike(elem.id)}}>
+                          <div class="flex mr-12 text-gray-700 text-sm mr-6 hover:bg-red-300" onClick={() => { createLike(elem.id) }}>
                             <svg
                               fill="none"
                               viewBox="0 0 24 24"
@@ -990,7 +1047,7 @@ const Posts = () => {
                     </div>{" "}
                   </div>
                 </div>
-              </>
+              </div>
             );
           })}
         </div>
